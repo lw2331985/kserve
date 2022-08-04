b724d1a8f57b4b6ae354287d8f80803f906d85d0
zhangjunze <zhangjunze>
2022-08-04 15:18:10 +0800

    lightgbm modified

--- python/lgbserver/lgbserver/model.py
+++ python/lgbserver/lgbserver/model.py
@@ -15,13 +15,15 @@
 import kserve
 import lightgbm as lgb
 from lightgbm import Booster
+import joblib
+import pathlib
 import os
 from typing import Dict
 import pandas as pd
 from kserve.model import ModelMissingError, InferenceError
 
-
-BOOSTER_FILE = "model.bst"
+MODEL_BASENAME = "model"
+MODEL_EXTENSIONS = [".joblib", ".pkl", ".pickle", ".txt", ".bst"]
 
 
 class LightGBMModel(kserve.Model):
@@ -36,21 +38,24 @@ class LightGBMModel(kserve.Model):
             self.ready = True
 
     def load(self) -> bool:
-        model_file = os.path.join(
-            kserve.Storage.download(self.model_dir), BOOSTER_FILE)
-        if not os.path.exists(model_file):
-            raise ModelMissingError(model_file)
-        self._booster = lgb.Booster(params={"nthread": self.nthread},
-                                    model_file=model_file)
+        model_path = pathlib.Path(kserve.Storage.download(self.model_dir))
+        paths = [os.path.join(model_path, MODEL_BASENAME + model_extension) for model_extension in MODEL_EXTENSIONS]
+        existing_paths = [path for path in paths if os.path.exists(path)]
+        if len(existing_paths) == 0:
+            raise ModelMissingError(model_path)
+        elif len(existing_paths) > 1:
+            raise RuntimeError('More than one model file is detected, '
+                               f'Only one is allowed within model_dir: {model_path}')
+        if existing_paths[0].endswith(".txt") or existing_paths[0].endswith(".bst"):
+            self._booster = lgb.Booster(params={"nthread": self.nthread}, model_file=existing_paths[0])
+        else:
+            self._booster = joblib.load(existing_paths[0])
         self.ready = True
         return self.ready
 
     def predict(self, request: Dict) -> Dict:
         try:
-            dfs = []
-            for input in request['inputs']:
-                dfs.append(pd.DataFrame(input, columns=self._booster.feature_name()))
-            inputs = pd.concat(dfs, axis=0)
+            inputs = pd.DataFrame(request['instances'], columns=self._booster.feature_name())
 
             result = self._booster.predict(inputs)
             return {"predictions": result.tolist()}
